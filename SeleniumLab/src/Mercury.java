559d1abb2a6e1cec9698c5583ebedb6915dfb77b
KoushikTeja <118737965+Koushik-77@users.noreply.github.com>
2024-06-25 09:42:54 +0530

    Update Mercury.java

--- SeleniumLab/src/Mercury.java
+++ SeleniumLab/src/Mercury.java
@@ -12,13 +12,13 @@ public class Mercury {
         driver.findElement(By.xpath("/html/body/nav[2]/div/div[2]/ul/li[1]")).click();
         driver.findElement(By.xpath("/html/body/nav[2]/div/div[2]/ul/li[1]/ul/li[2]/a")).click();
         Thread.sleep(2000);
-        driver.findElement(By.id("acc_first_name")).sendKeys("saketh");
-        driver.findElement(By.id("acc_last_name")).sendKeys("pulluru");
+        driver.findElement(By.id("acc_first_name")).sendKeys("yourname");
+        driver.findElement(By.id("acc_last_name")).sendKeys("yourname");
 
-        driver.findElement(By.id("acc_user_email")).sendKeys("pullurusaketh@gmail.com");
-        driver.findElement(By.id("acc_user_password")).sendKeys("saketh@2006");
-        driver.findElement(By.id("acc_user_passconf")).sendKeys("saketh@2006");
-        driver.findElement(By.id("acc_mobile_no")).sendKeys("7337250206");
+        driver.findElement(By.id("acc_user_email")).sendKeys("example@gmail.com");
+        driver.findElement(By.id("acc_user_password")).sendKeys("example@2006");
+        driver.findElement(By.id("acc_user_passconf")).sendKeys("example@2006");
+        driver.findElement(By.id("acc_mobile_no")).sendKeys("00000000000");
         driver.findElement(By.xpath("/html/body/div[4]/div/div/div[2]/form/button")).click();
         driver.quit();
     }
