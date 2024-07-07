2a83d9391b5a8e15e8e0d879b814e7df0a31651d
KoushikTeja <118737965+Koushik-77@users.noreply.github.com>
2024-07-07 14:01:15 +0530

    Update SearchCmrit.java

--- SeleniumLab/src/SearchCmrit.java
+++ SeleniumLab/src/SearchCmrit.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SearchCmrit {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saket\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
