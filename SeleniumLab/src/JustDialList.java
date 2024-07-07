7c76860b1d535e57696dc16ec51bc05e08651188
KoushikTeja <118737965+Koushik-77@users.noreply.github.com>
2024-07-07 13:59:09 +0530

    Update JustDialList.java

--- SeleniumLab/src/JustDialList.java
+++ SeleniumLab/src/JustDialList.java
@@ -1,17 +1,17 @@
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class JustDialList {
+public class Week9 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saket\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+	    WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.get("https://www.justdial.com/");
-        driver.findElement(By.xpath("/html/body/div/div/section/section[2]/div/div/div[2]/a[8]")).click();
-        driver.manage().deleteAllCookies();
-        driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[2]/ul/li[1]/a")).click();
-        driver.manage().deleteAllCookies();
+        driver.get("https://www.justdial.com/Hyderabad/Bakeries/nct-10033880");
+        List<WebElement> m = driver.findElements(By.xpath("/html/body/div/section/section/div/div[5]"));
+        for(int i = 0; i< m.size(); i++) {
+            String s = m.get(i).getText();
+            System.out.println("Text is: " + s);
+        }
     }
 }
