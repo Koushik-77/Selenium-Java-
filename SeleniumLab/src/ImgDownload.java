f9527c510de4488196b4fff2889bfc9c3dbe3a51
KoushikTeja <118737965+Koushik-77@users.noreply.github.com>
2024-07-07 13:59:50 +0530

    Update ImgDownload.java

--- SeleniumLab/src/ImgDownload.java
+++ SeleniumLab/src/ImgDownload.java
@@ -10,7 +10,6 @@ import java.awt.event.KeyEvent;
 
 public class ImgDownload {
     public static void main(String[] args) throws AWTException, InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saket\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
